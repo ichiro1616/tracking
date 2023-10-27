100bf9ef1c7931137f2edbb50d4a43c53798b19c
ichiro1616 <ichiro.nakagawa1616@gmail.com>
2023-10-27 18:55:03 +0900

    python rgbでキーポイント周りの平均を取得。その後hsvに。

--- python/test_mov_normalize.py
+++ python/test_mov_normalize.py
@@ -18,13 +18,18 @@ def write_list_2d(sheet, l_2d, start_row, start_col):
     row_count = 0
     col_count = 0 
 
-    for y, row_data in enumerate(l_2d):
+    for y, row_data in enumerate(l_2d): #各keypointを列に指定
         # for x, cell_data in enumerate(row_data):
             if col_count == 25:
                 row_count += 1
                 col_count = 0
             sheet.cell(row=start_row + row_count, column=start_col + col_count, value=l_2d[y])
             col_count += 1
+    # for y, row_data in enumerate(l_2d): #1列にすべて表示
+    #     # for x, cell_data in enumerate(row_data):
+    #         sheet.cell(row=start_row + y, column=start_col, value=l_2d[y])
+
+    
 
 try:
     # Import Openpose (Windows/Ubuntu/OSX)
@@ -48,7 +53,7 @@ try:
     params = dict()
     params["model_folder"] = "../models/"
 
-    moviefile = "hinata_front.mov"
+    moviefile = "hinata_side.mov"
     # params["render_pose"] = 2  #レンダーにGPUを使用する
     # params["ip_camera"] = 'rtsp://root:P7CEqNF5ui8e@10.5.5.145:554/live1s1.sdp'
     params["video"] = "../examples/movie/" + moviefile  #ファイルパス
@@ -98,62 +103,38 @@ try:
         print(counter, "枚目")
         counter += 1
         # Pop frame
-        if counter !=  2700:
+        if counter !=  240:
             datumProcessed = op.VectorDatum()
             if opWrapper.waitAndPop(datumProcessed):
                 if not args[0].no_display:
                     datum = datumProcessed[0]
                     
                     imageToProcess = datum.cvInputData
-                    # print(imageToProcess.shape)
-                    # datum = op.Datum()
-                    # imageToProcess = cv2.imread(datumProcessed[0])
-                    # datum.cvInputData = imageToProcess
-                    # opWrapper.emplaceAndPop(op.VectorDatum([datum]))
 
-
-
+                    #首と腰のキーポイントを利用して画像をリサイズする。
                     keypoint_neck_x = int(datum.poseKeypoints[0][1][0])
                     keypoint_neck_y = int(datum.poseKeypoints[0][1][1])
 
                     keypoint_hip_x = int(datum.poseKeypoints[0][8][0])
                     keypoint_hip_y = int(datum.poseKeypoints[0][8][1])
-                    # print("首", "keypoint_x",int(keypoint_neck_x), "keypoint_y",int(keypoint_neck_y))
-                    # print("腰", "keypoint_x",int(keypoint_hip_x), "keypoint_y",int(keypoint_hip_y))
 
-                    sekitui = math.sqrt(abs(keypoint_hip_x - keypoint_neck_x) ** 2  + abs(keypoint_hip_y - keypoint_neck_y) ** 2)
-                    # print(sekitui)
-                    threshold = 80
+                    sekitui = math.sqrt((keypoint_hip_x - keypoint_neck_x) ** 2  + (keypoint_hip_y - keypoint_neck_y) ** 2)
+
+                    threshold = 80.0 #首から腰までを80pxにするという基準値
+
                     if sekitui != 0:
                         resize_rate = round(threshold / sekitui, 2)
-                        # print(resize_rate)
-                        # print(imageToProcess.shape[0], imageToProcess.shape[1])
-                    
-                        # imageToProcess = cv2.imread(imagePath)
                         img_resize = cv2.resize(imageToProcess, (int(imageToProcess.shape[1] * resize_rate), int(imageToProcess.shape[0] * resize_rate)))
 
                         datum.cvInputData = img_resize
-                        # opWrapper.emplaceAndPop(op.VectorDatum([datum]))
-
-                        HSV_img_save = cv2.cvtColor(imageToProcess,cv2.COLOR_BGR2HSV)
-
-                        HSV_img = cv2.cvtColor(img_resize,cv2.COLOR_BGR2HSV)
-                        h, s, v = cv2.split(HSV_img)
-                        # print("H:", h, "S:", s, "V:", v)
-
-
                         keypoint_array = [] #x, y
-                        # print("h_shape:",h.shape)
-                
                         
                         for i in range(25):
-                    
                             prob = int(datum.poseKeypoints[0][i][2] * 100)
 
-                            h_array = []
-                            px_sum_h = 0
                             width_r =10 #半径
                             height_r =10 #半径
+
                             if(prob == 0):
                                 keypoint_array.append([-1, -1])
                                 print(-10000000)
@@ -163,35 +144,24 @@ try:
                             else:
                                 keypoint_x = int(datum.poseKeypoints[0][i][0] * resize_rate)
                                 keypoint_y = int(datum.poseKeypoints[0][i][1] * resize_rate)
-                                keypoint_array.append([int(keypoint_x), int(keypoint_y)])
+                                keypoint_array.append([keypoint_x, keypoint_y])
+                                img_area = img_resize[keypoint_y - height_r : keypoint_y + height_r, keypoint_x - width_r : keypoint_x + width_r]
 
-                    
-                                for j in range(-height_r, height_r, 1):
-                                    for k in range(-width_r, width_r, 1):
-                                        # print(keypoint_y + j)
-                                        if (keypoint_y + j) >= h.shape[0] or (keypoint_x + k) >= h.shape[1]:
-                                            px = 0
-                                        else:
-                                            px = h[keypoint_y + j][keypoint_x + k]
-                                        # print(px)
-                                        px_sum_h += px
-                                        h_array.append(px)
-                                if len(h_array) == 0:
-                                    px_ave_h = -1
-                                else:
-                                    px_ave_h = int(px_sum_h / len(h_array))
-
-                                # print("px_sum_h",int(px_sum_h))
-                                # print("h_array",len(h_array))
+                                rsum,gsum,bsum = 0.0,0.0,0.0
                                 
-                                print(px_ave_h)
-                                list_output.append(px_ave_h)
-                    # else:
-                        # break;      
+
+                                #画像の範囲指定をしたimg_areaからRGBをそれぞれ2次元配列で全px分取得。それをravelで1次元化してから平均を取得している
+                                ravg = np.ravel(img_area[:, :, 2]).mean() 
+                                gavg = np.ravel(img_area[:, :, 1]).mean()
+                                bavg = np.ravel(img_area[:, :, 0]).mean()
+
+                                hsv = cv2.cvtColor(np.array([[[bavg, gavg, ravg]]], dtype=np.uint8), cv2.COLOR_BGR2HSV)[0][0]
+                                print("H:", hsv[0])
+                                list_output.append(hsv[0])
 
 
-                    cv2.imshow("OpenPose 1.7.0 - Tutorial Python API", datum.cvInputData)
-                    outimg_files = [HSV_img_save]
+
+                    cv2.imshow("OpenPose 1.7.0 - Tutorial Python API", datum.cvOutputData)
                     # cv2.imshow("OpenPose 1.7.0 - Tutorial Python API", HSV_img)
 
 
@@ -201,23 +171,17 @@ try:
                 break
         else:
             break;    
-    # # 動画作成
-    # fourcc = cv2.VideoWriter_fourcc('m','o','v', 'v')
-    # video  = cv2.VideoWriter("../examples/movie/outputHSV_" + moviefile, fourcc, 20.0)
 
-    # for img_file in outimg_files:
-    #     img = cv2.imread(img_file)
-    #     video.write(img)
+    excel_path = r"C:\Users\isapo\openpose-1.7.0-binaries-win64-gpu-python3.7-flir-3d_recommended\openpose\examples\20x20.xlsx"
+    # excel_path = r"C:\Users\isapo\openpose-1.7.0-binaries-win64-gpu-python3.7-flir-3d_recommended\openpose\examples\distance.xlsx"
 
-    # video.release()
-    # excel_path = r"C:\Users\isapo\openpose-1.7.0-binaries-win64-gpu-python3.7-flir-3d_recommended\openpose\examples\20x20.xlsx"
-    # wb = openpyxl.load_workbook(excel_path)
-    # sheet = wb.worksheets[0]
+    wb = openpyxl.load_workbook(excel_path)  
+    sheet = wb.worksheets[0]
 
-    # write_list_2d(sheet, list_output, 3,2) #行、列
+    write_list_2d(sheet, list_output, 3,2) #行、列
 
-    # wb.save(excel_path)
-    # wb.close()
+    wb.save(excel_path)
+    wb.close()
 
 except Exception as e:
     print(e)
